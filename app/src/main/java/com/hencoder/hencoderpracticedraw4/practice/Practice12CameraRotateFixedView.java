79945b26a23f48bb1c2bec0afd7ed497d085f486
wdjhzw <wdjhzw13@gmail.com>
2017-10-08 03:25:52 +0800

    update

--- app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw4/practice/Practice12CameraRotateFixedView.java
@@ -3,7 +3,9 @@ package com.hencoder.hencoderpracticedraw4.practice;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Camera;
 import android.graphics.Canvas;
+import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.support.annotation.Nullable;
@@ -17,6 +19,8 @@ public class Practice12CameraRotateFixedView extends View {
     Bitmap bitmap;
     Point point1 = new Point(200, 200);
     Point point2 = new Point(600, 200);
+    Camera camera = new Camera();
+    Matrix matrix = new Matrix();
 
     public Practice12CameraRotateFixedView(Context context) {
         super(context);
@@ -38,7 +42,30 @@ public class Practice12CameraRotateFixedView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
+        int centerX = bitmap.getWidth() / 2, centerY = bitmap.getHeight() / 2;
+
+        matrix.reset();
+        camera.save();
+        camera.rotateX(30);
+        camera.getMatrix(matrix);
+        camera.restore();
+        matrix.preTranslate(-(point1.x + centerX), -(point1.y + centerY));
+        matrix.postTranslate((point1.x + centerX), (point1.y + centerY));
+        canvas.save();
+        canvas.concat(matrix);
         canvas.drawBitmap(bitmap, point1.x, point1.y, paint);
+        canvas.restore();
+
+        matrix.reset();
+        camera.save();
+        camera.rotateY(30);
+        camera.getMatrix(matrix);
+        camera.restore();
+        canvas.save();
+        matrix.preTranslate(-(point2.x + centerX), -(point2.y + centerY));
+        matrix.postTranslate((point2.x + centerX), (point2.y + centerY));
+        canvas.concat(matrix);
         canvas.drawBitmap(bitmap, point2.x, point2.y, paint);
+        canvas.restore();
     }
 }
